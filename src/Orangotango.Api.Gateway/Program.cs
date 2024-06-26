b4ff3a59b566fe56fd01d5314181784766c99a3c
Wesley Costa <wesley_costa@outlook.com>
2024-06-26 16:10:55 -0300

    fix

--- src/Orangotango.Api.Gateway/Program.cs
+++ src/Orangotango.Api.Gateway/Program.cs
@@ -11,6 +11,7 @@ var app = new WebAppBuilder()
             .SetBasePath(env.ContentRootPath)
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"ocelot.{env.EnvironmentName}.json", optional: false, reloadOnChange: true)
             .AddEnvironmentVariables();
     })
